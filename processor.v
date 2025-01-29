066ce5cc880ff2f77d7b860a001925a8d5559a54
David Garcia Gonzalez <nnguye24@nd.edu>
2025-01-29 16:20:17 -0500

    Updated FSM processor and added verification module
    processor.v no longer relies on user input to determine a read/write operation

--- processor.v
+++ processor.v
@@ -8,6 +8,12 @@
     input wire tx_done,
     input wire rx_done,
     
+    input wire dut_start_address,   // address will come from verification.v 
+    input wire dut_end_address, // DUT address is the address for DUT ouputs
+    input wire input_start_address, // input address is the address for the DUT inputs. 
+    input wire input_end_address,
+    input wire [7:0] command, // command to determine read/write will be received from verification.
+    
     output reg bram_mode,
     output reg [15:0] bram_address,
     output reg [7:0] bram_byte_write,
@@ -16,13 +22,13 @@
     output reg rx_trigger
     );
     
-    localparam STATE_WAITING_FOR_COMMAND = 0;
+
     localparam STATE_PROCESSING_COMMAND = 1;
     localparam STATE_READ = 2;
     localparam STATE_WRITE_WAITING_FOR_DATA = 3;
     localparam STATE_WRITE = 4;
     
-    reg [7:0] command; // 5 bytes: read(0x00)/write(0x01); start address(2); end address(2)
+   
     reg [2:0] command_byte_counter;
     reg [2:0] state;
     reg [15:0] start_address;
@@ -38,9 +44,9 @@
         tx_trigger <= 0;
         rx_trigger <= 0;
         
-        command <= 0;
+
         command_byte_counter <= 0;
-        state <= STATE_WAITING_FOR_COMMAND;
+
         start_address <= 0;
         end_address <= 0;
         bram_ready <= 0;
@@ -49,56 +55,26 @@
     
     always @(posedge clk) begin
         case (state)
-            STATE_WAITING_FOR_COMMAND: begin
-                if (rx_trigger) begin
-                    rx_trigger <= 0;
-                end
-                // for commands 
-                else if (rx_done) begin 
-                    if (command_byte_counter == 0) begin    // determines if write or read
-                        command <= rx_byte;
-                        rx_trigger <= 1;
-                        command_byte_counter <= command_byte_counter + 1;
-                    end
-                    else if (command_byte_counter == 1) begin   // we can only do 4 bits at a time so we send addresses with 2 inputs
-                        start_address[15:8] <= rx_byte;
-                        rx_trigger <= 1;
-                        command_byte_counter <= command_byte_counter + 1;
-                    end
-                    else if (command_byte_counter == 2) begin
-                        start_address[7:0] <= rx_byte;
-                        rx_trigger <= 1;
-                        command_byte_counter <= command_byte_counter + 1;                        
-                    end
-                    else if (command_byte_counter == 3) begin
-                        end_address[15:8] <= rx_byte;
-                        rx_trigger <= 1;
-                        command_byte_counter <= command_byte_counter + 1;
-                    end
-                    else if (command_byte_counter == 4) begin
-                        end_address[7:0] <= rx_byte;
-                        command_byte_counter <= 0;
-                        state <= STATE_PROCESSING_COMMAND;
-                    end
-                end
-            end
+
             STATE_PROCESSING_COMMAND: begin
                 if (!rx_done) begin
                     rx_trigger <= 0;
-                    if (command == 8'h00) state <= STATE_READ;
+                    if (command == 8'h00) state <= STATE_READ;  // command will be received from verification.v!
                     else if (command == 8'h01) state <= STATE_WRITE_WAITING_FOR_DATA;
-                    else state <= STATE_WAITING_FOR_COMMAND;
+                    else state <= STATE_PROCESSING_COMMAND;
                 end
                 else rx_trigger <= 1;
             end
             STATE_READ: begin   // reads from memory location and transmit to computer through UART
+                start_address <= dut_start_address;
+                end_address <= dut_end_address;
                 if (tx_trigger) begin   //
                     tx_trigger <= 0;
                     if (start_address > end_address) begin // done transmitting, send back to WAITING FOR COMMAND
                         bram_ready <= 0;
-                        start_address <= 0;
+                        start_address <= 0; // resets addresses
                         end_address <= 0;
-                        state <= STATE_WAITING_FOR_COMMAND;
+                        state <= STATE_PROCESSING_COMMAND;  // send back to 
                     end
                 end
                 else if (tx_ready) begin // tx_ready indicates we are ready to transmit our byte, 
@@ -128,6 +104,8 @@
             // we are cycling through these states right as we receive the bits at a low speed. Since we cycle fast, we don't need to care
             // about missing timing. WE ARE SIGNIFICANTLY FASTER THAN THE BAUD RATE
             STATE_WRITE: begin
+                start_address <= input_start_address;
+                end_address <= input_end_address;
                 if (rx_trigger) begin
                     if (start_address > end_address) begin
                         bram_mode <= 0; // extra precaution
@@ -135,7 +113,7 @@
                         rx_trigger <= 1;
                         start_address <= 0;
                         end_address <= 0;
-                        state <= STATE_WAITING_FOR_COMMAND;
+                        state <= STATE_PROCESSING_COMMAND;  // send back to check if read/write
                     end
                     else rx_trigger <= 0;
                 end
@@ -159,7 +137,7 @@
                 tx_trigger <= 0;
                 start_address <= 0;
                 end_address <= 0;
-                state <= STATE_WAITING_FOR_COMMAND;
+                state <= STATE_PROCESSING_COMMAND;  
             end
         endcase
     end
---
+++ verification.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/22/2025 04:22:30 PM
+// Design Name: 
+// Module Name: verification
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module verification(
+    input clk,
+    input wire tx_done,
+    input wire rx_done,
+    
+    output reg [15:0] dut_start_address,
+    output reg [15:0] dut_end_address,
+    output reg [15:0] input_start_address,
+    output reg [15:0] input_end_address,
+    output reg [7:0] command,   // to send to processor to determine if write/read from memory location
+    output reg [7:0] dut_in // logic inputs sent out to DUT
+    
+    );
+    
+    localparam TX_DUT = 0;
+    localparam RX_DUT = 1;
+    localparam RX_INPUT = 2;
+    
+//    localparam WAIT_IN = 0;
+//    localparam WRITE_IN = 1;
+//    localparam READ_IN = 2;
+//    localparam TX_IN= 3;
+//    localparam WAIT_OUT = 4;
+//    localparam RX_OUT = 5;
+//    localparam WRITE_OUT = 6;
+    
+    reg [2:0] state;
+    // read: 8h00 , write 8h01 
+    always @(posedge clk) begin
+    
+        case (state) 
+        TX_DUT: begin
+        
+            end
+        RX_DUT: begin
+        
+            end
+        RX_INPUT: begin
+        
+            end
+        
+    end
+endmodule
